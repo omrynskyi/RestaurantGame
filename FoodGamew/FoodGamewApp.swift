570189c0a6ca5d2054375125a5364f86ab6c2e14
Oleg <omrynskyi@gmail.com>
2024-02-15 17:27:36 -0800

    Initial Commit

---
+++ FoodGamew/FoodGamewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodGamewApp.swift
+//  FoodGamew
+//
+//  Created by Oleg Mrynskyi on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodGamewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
